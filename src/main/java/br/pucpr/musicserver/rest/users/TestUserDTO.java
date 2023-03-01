7473df6e969e22a90de5cd1af76e4abc0621348e
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-03-01 20:22:36 -0300

    feat: using tests and config files

---
+++ src/main/java/br/pucpr/musicserver/rest/users/TestUserDTO.java
@@ -0,0 +1,12 @@
+package br.pucpr.musicserver.rest.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestUserDTO {
+    private String token;
+}
